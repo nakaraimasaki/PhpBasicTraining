24ba6418e30b0f6e773af9b6c9c8b63e4b6dbbf9
nakarai <masakinito418@gmail.com>
2016-05-09 18:03:27 +0900

    20160509

---
+++ 20160509/012_difine2.php
@@ -0,0 +1,6 @@
+<?php
+define("kazu", 87);
+echo kazu ."\n";
+define("kazu", 99);
+echo kazu ."\n";
+?>
---
+++ 20160509/017_ketsugou2.php
@@ -0,0 +1,7 @@
+<?php
+// 変数に文字列を格納
+$str_name1 = "半井";
+$str_name2 = "雅樹";
+// 文字列を連結して出力
+echo $str_name1.$str_name2. "だよ。\n";
+?>
