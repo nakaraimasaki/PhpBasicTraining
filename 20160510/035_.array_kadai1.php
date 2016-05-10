ebfd036776444c939a97cecb75100606d1d9dd5e
nakarai <masakinito418@gmail.com>
2016-05-10 17:32:20 +0900

    a

---
+++ 20160510/035_.array_kadai1.php
@@ -0,0 +1,10 @@
+<?php
+$d[] = "C言語";
+$d[] = "MySQL";
+$d[] = "PHP";
+$d[] = "AKB";
+$d[] = "Java";
+foreach ( $d as $value ) {
+  echo $value ."<br/>";
+}
+?>
---
+++ 20160510/036_.array_kadai2.php
@@ -0,0 +1,15 @@
+<?php
+$d[] = "C言語";
+$d[] = "MySQL";
+$d[] = "PHP";
+$d[] = "AKB";
+$d[] = "Java";
+foreach ( $d as $value ) {
+  echo $value ."<br/>";
+}
+$d[5]="研修";
+foreach ( $d as $value ) {
+  echo $value ."<br/>";
+}
+
+?>
